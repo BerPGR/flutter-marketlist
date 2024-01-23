8ef56c23a618bef756578be504b83dfbdf85a862
Bernardo Matuchewski <bernardoPGR01@gmail.com>
2024-01-23 11:04:37 -0300

    Finished categories and subcategories

--- lib/model/categories.dart
+++ lib/model/categories.dart
@@ -6,3 +6,37 @@ class Categories {
 
   const Categories({required this.name, required this.subcategories});
 }
+
+Categories acougue =
+    Categories(name: "Açougue", subcategories: [aves, bovinos]);
+
+Categories bebidas = Categories(name: "Bebidas", subcategories: [
+  aguas,
+  cafes,
+  chas,
+  cervejas,
+  refrigerantes,
+  drinks,
+  sucos,
+  vinhos
+]);
+
+Categories frioEmbutidos =
+    Categories(name: "Frios & Embutidos", subcategories: [embutidos, queijos]);
+
+Categories hortifruti = Categories(
+    name: "Hortifruti",
+    subcategories: [frutas, verduras, legumes, temperos, ovos]);
+
+Categories laticinios = Categories(
+    name: "Laticínios",
+    subcategories: [leites, mantemarga, cremRequeijao, iogurtes]);
+
+Categories mercearia = Categories(name: "Mercearia", subcategories: [
+  alimentosBasicos,
+  biscSnacks,
+  cereaisFarinha,
+  conserEnla,
+  docesGeleias,
+  deterLimpeza
+]);
--- lib/model/subcategories.dart
+++ lib/model/subcategories.dart
@@ -32,7 +32,7 @@ Subcategories cervejas = Subcategories(name: "Cervejas", products: [
   "Puro Malte",
   "Ipa",
   "Super Premium",
-  "Outras"
+  "Outro"
 ]);
 
 Subcategories refrigerantes =
@@ -45,16 +45,224 @@ Subcategories drinks = Subcategories(name: "Destilados e Drinks", products: [
   "Saquê",
   "Vodka",
   "Cachaça",
-  "Gin"
+  "Gin",
+  "Outro"
 ]);
 
 Subcategories vinhos = Subcategories(
     name: "Vinhos", products: ["Vinho Tinto", "Vinho Branco", "Vinho Rose"]);
 
-Subcategories embutidos = Subcategories(
-    name: "Embutidos",
-    products: ["Salame", "Presunto", "Peito de Peru", "Salsicha", "Mortadela"]);
+Subcategories embutidos = Subcategories(name: "Embutidos", products: [
+  "Salame",
+  "Presunto",
+  "Peito de Peru",
+  "Salsicha",
+  "Mortadela",
+  "Outro"
+]);
 
 Subcategories queijos = Subcategories(
     name: "Queijos",
     products: ["Mussarela", "Parmesão", "Gorgonzola", "Queijo minas", "Outro"]);
+
+Subcategories aves = Subcategories(
+    name: "Aves", products: ["Frango", "Temperado", "Sem tempero", "Outro"]);
+
+Subcategories bovinos = Subcategories(name: "Bovinos", products: [
+  "Filé Mignon",
+  "Contrafilé",
+  "Picanha",
+  "Alcatra",
+  "Hamburguer",
+  "Costela",
+  "Patinho",
+  "Musculo",
+  "Carne Seca",
+  "Acem",
+  "Paleta",
+  "Outro"
+]);
+
+Subcategories frutas = Subcategories(name: "Frutas", products: [
+  "Maçã",
+  "Kiwi",
+  "Coco",
+  "Laranja",
+  "Morango",
+  "Melão",
+  "Pera",
+  "Manga",
+  "Tangerina",
+  "Abacaxi",
+  "Limão",
+  "Abacate",
+  "Goiaba",
+  "Cereja",
+  "Pêssego",
+  "Maracujá",
+  "Ameixa",
+  "Melancia",
+  "Outro"
+]);
+
+Subcategories legumes = Subcategories(name: "Legumes", products: [
+  "Tomate",
+  "Abóbora",
+  "Milho",
+  "Batata",
+  "Abobrinha",
+  "Pimentão",
+  "Alho",
+  "Cebola",
+  "Batata doce",
+  "Berinjela",
+  "Mandioca",
+  "Pepino",
+  "Beterraba",
+  "Chuchu",
+  "Outro"
+]);
+
+Subcategories verduras = Subcategories(name: "Verduras", products: [
+  "Alface",
+  "Couve",
+  "Rucula",
+  "Agrião",
+  "Brócolis",
+  "Espinafre",
+  "Couve-Flor",
+  "Repolho",
+  "Acelga",
+  "Outro"
+]);
+
+Subcategories temperos = Subcategories(name: "Temperos", products: [
+  "Sal",
+  "Cheiro-verde",
+  "Canela",
+  "Páprica",
+  "Chimichurri",
+  "Nóz moscada",
+  "Alho, cebola e salsa",
+  "Orégano",
+  "Coloral",
+  "Pimenta",
+  "Manjericão",
+  "Hortelã",
+  "Tomilho",
+  "Salsão",
+  "Manjerão",
+  "Salsão",
+  "Gengibre",
+  "Açafrão",
+  "Salsinha",
+  "Cebolinha",
+  "Outro"
+]);
+
+Subcategories ovos = Subcategories(name: "Ovos", products: [
+  "Ovo",
+  "Ovo de codorna",
+]);
+
+Subcategories leites = Subcategories(name: "Leites", products: [
+  "Leite integral",
+  "Leite desnatado",
+  "Leite semi-desnatado",
+  "Leite em pó",
+  "Leite pasteurizado",
+  "Leite vida longa",
+  "Leite de coco",
+]);
+
+Subcategories mantemarga = Subcategories(
+    name: "Manteigas & Margarinas",
+    products: ["Manteiga com sal", "Manteiga sem sal", "Margarina", "Outro"]);
+
+Subcategories cremRequeijao =
+    Subcategories(name: "Requeijão e outros", products: [
+  "Requeijão",
+  "Cream Cheese",
+  "Creme de Queijo",
+  "Creme de leite",
+  "Leite condensado",
+  "Chantilly"
+]);
+
+Subcategories iogurtes = Subcategories(
+    name: "Iogurtes", products: ["Iogurte", "Kefir", "Leite fermentado"]);
+
+Subcategories alimentosBasicos = Subcategories(
+    name: "Alimentos Básicos",
+    products: [
+      "Arroz",
+      "Açúcar",
+      "Adoçante",
+      "Feijão",
+      "Farofa",
+      "Óleo",
+      "Mel",
+      "Outro"
+    ]);
+
+Subcategories biscSnacks = Subcategories(name: "Biscoitos & Snacks", products: [
+  "Biscoito doce",
+  "Biscoito salgado",
+  "Batata palha",
+  "Batata chips",
+  "Pipoca",
+  "Chocolate",
+  "Alfajor",
+  "Pão de mel",
+  "Outro"
+]);
+
+Subcategories cereaisFarinha =
+    Subcategories(name: "Cereais & Farinhas", products: [
+  "Aveia",
+  "Tapioca",
+  "Linhaça",
+  "Chia",
+  "Quinoa",
+  "Granola",
+  "Outro",
+]);
+
+Subcategories conserEnla = Subcategories(
+    name: "Conservas & Enlatados",
+    products: ["Azeitona", "Palmito", "Milho", "Cogumelo", "Outro"]);
+
+Subcategories docesGeleias = Subcategories(name: "Doces & Gelerias", products: [
+  "Doce de leite",
+  "Goiabada",
+  "Amendoim",
+  "Gelatina",
+  "Bananada",
+  "Geleia",
+  "Outro"
+]);
+
+Subcategories deterLimpeza =
+    Subcategories(name: "Detergente & Limpeza", products: [
+  "Detergente",
+  "Sabão em pó",
+  "Sabão líquido",
+  "Amaciante",
+  "Lava roupas",
+  "Absorvente",
+  "Desinfetante",
+  "Pasta de dente",
+  "Sabonete",
+  "Cotonete",
+  "Fio-dental",
+  "Desodorante",
+  "Água sanitária",
+  "Álcool",
+  "Papel higiênico",
+  "Palha de aço",
+  "Guardanapo",
+  "Esponja",
+  "saco de lixo",
+  "Lustra-móveis",
+  "Outro",
+]);
