599c1f8ebf6a064c202ea59b278695ce15acde2c
Bernardo Matuchewski <bernardoPGR01@gmail.com>
2024-01-26 10:12:37 -0300

    Feat: Styles for register and login screen

--- lib/screens/login_register_screen.dart
+++ lib/screens/login_register_screen.dart
@@ -18,61 +18,108 @@ class _LoginScreenState extends State<LoginScreen> {
     return Scaffold(
         body: Center(
             child: Padding(
-      padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            'Bem-vindo a sua lista de mercado!',
-            textAlign: TextAlign.center,
-          ),
-          TextField(
-            controller: _emailController,
-            decoration: InputDecoration(
-              labelText: 'E-mail',
-              enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(100),
-                  borderSide: BorderSide(width: 2, color: Color(0xFF1565C0))),
-              focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(100),
-                  borderSide: BorderSide(width: 2, color: Color(0xFF1565C0))),
-            ),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          TextField(
-            controller: _passwordController,
-            decoration: InputDecoration(
-              labelText: 'Senha',
-              enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(100),
-                  borderSide: BorderSide(width: 2, color: Color(0xFF1565C0))),
-              focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(100),
-                  borderSide: BorderSide(width: 2, color: Color(0xFF1565C0))),
-            ),
-          ),
-          SizedBox(
-            height: 25,
-          ),
-          Container(
-            width: double.infinity,
-            child: ElevatedButton(
-                onPressed: () => _auth.signInWithEmailAndPassword(
-                    _emailController.text, _passwordController.text),
-                child: Text('Login')),
-          ),
-          Container(
-            width: double.infinity,
-            child: ElevatedButton(
-                onPressed: () => Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => RegisterScreen())),
-                child: Text('Register')),
-          )
-        ],
-      ),
-    )));
+                padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                child: SingleChildScrollView(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(bottom: 24),
+                        child: Text(
+                          'Bem-vindo a sua lista de mercado!',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              fontSize: 24, fontWeight: FontWeight.w600),
+                        ),
+                      ),
+                      TextField(
+                        controller: _emailController,
+                        decoration: InputDecoration(
+                          labelText: 'E-mail',
+                          enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(100),
+                              borderSide: BorderSide(
+                                  width: 2, color: Color(0xFF1565C0))),
+                          focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(100),
+                              borderSide: BorderSide(
+                                  width: 2, color: Color(0xFF1565C0))),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      TextField(
+                        controller: _passwordController,
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          labelText: 'Senha',
+                          enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(100),
+                              borderSide: BorderSide(
+                                  width: 2, color: Color(0xFF1565C0))),
+                          focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(100),
+                              borderSide: BorderSide(
+                                  width: 2, color: Color(0xFF1565C0))),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 25,
+                      ),
+                      Container(
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          onPressed: () => _auth.signInWithEmailAndPassword(
+                              _emailController.text, _passwordController.text),
+                          child: Text(
+                            'LOGIN',
+                            style: TextStyle(
+                                fontSize: 20, color: Color(0xFFFFFFFF)),
+                          ),
+                          style: ElevatedButton.styleFrom(
+                              elevation: 0,
+                              backgroundColor: Color(0xFF1565C0),
+                              padding: EdgeInsets.all(10),
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(100),
+                                  side: BorderSide(
+                                      width: 2, color: Color(0xFF1565C0)))),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(20),
+                        child: Text(
+                          'Ou registre-se',
+                          style: TextStyle(
+                              fontWeight: FontWeight.w600, fontSize: 24),
+                        ),
+                      ),
+                      Container(
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          onPressed: () => Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => RegisterScreen())),
+                          child: Text(
+                            'REGISTRE',
+                            style: TextStyle(
+                                fontSize: 20, color: Color(0xFF1565C0)),
+                          ),
+                          style: ElevatedButton.styleFrom(
+                              elevation: 0,
+                              backgroundColor: Colors.transparent,
+                              padding: EdgeInsets.all(10),
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(100),
+                                  side: BorderSide(
+                                      width: 2, color: Color(0xFF1565C0)))),
+                        ),
+                      )
+                    ],
+                  ),
+                ))));
   }
 }
 
@@ -93,78 +140,126 @@ class _RegisterScreenState extends State<RegisterScreen> {
     return Scaffold(
         body: Center(
             child: Padding(
-      padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Text(
-            'Registre-se para começar!',
-            textAlign: TextAlign.center,
-          ),
-          TextField(
-            controller: _emailController,
-            decoration: InputDecoration(
-              labelText: 'E-mail',
-              enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(100),
-                  borderSide: BorderSide(width: 2, color: Color(0xFF1565C0))),
-              focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(100),
-                  borderSide: BorderSide(width: 2, color: Color(0xFF1565C0))),
-            ),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          TextField(
-            controller: _passwordController,
-            obscureText: true,
-            decoration: InputDecoration(
-              labelText: 'Senha',
-              enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(100),
-                  borderSide: BorderSide(width: 2, color: Color(0xFF1565C0))),
-              focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(100),
-                  borderSide: BorderSide(width: 2, color: Color(0xFF1565C0))),
-            ),
-          ),
-          SizedBox(
-            height: 25,
-          ),
-          TextField(
-            controller: _nameController,
-            decoration: InputDecoration(
-              labelText: 'Nome',
-              enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(100),
-                  borderSide: BorderSide(width: 2, color: Color(0xFF1565C0))),
-              focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(100),
-                  borderSide: BorderSide(width: 2, color: Color(0xFF1565C0))),
-            ),
-          ),
-          SizedBox(
-            height: 25,
-          ),
-          Container(
-            width: double.infinity,
-            child: ElevatedButton(
-                onPressed: () => _auth.registerWithEmailAndPassword(
-                    _emailController.text,
-                    _passwordController.text,
-                    _nameController.text),
-                child: Text('Registrar')),
-          ),
-          Text('Ou faça login'),
-          Container(
-            width: double.infinity,
-            child: ElevatedButton(
-                onPressed: () => Navigator.pop(context), child: Text('Login')),
-          )
-        ],
-      ),
-    )));
+                padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                child: SingleChildScrollView(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(bottom: 24),
+                        child: Text(
+                          'Registre-se para começar!',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              fontSize: 24, fontWeight: FontWeight.w600),
+                        ),
+                      ),
+                      TextField(
+                        controller: _emailController,
+                        decoration: InputDecoration(
+                          labelText: 'E-mail',
+                          enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(100),
+                              borderSide: BorderSide(
+                                  width: 2, color: Color(0xFF1565C0))),
+                          focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(100),
+                              borderSide: BorderSide(
+                                  width: 2, color: Color(0xFF1565C0))),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      TextField(
+                        controller: _passwordController,
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          labelText: 'Senha',
+                          enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(100),
+                              borderSide: BorderSide(
+                                  width: 2, color: Color(0xFF1565C0))),
+                          focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(100),
+                              borderSide: BorderSide(
+                                  width: 2, color: Color(0xFF1565C0))),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 25,
+                      ),
+                      TextField(
+                        controller: _nameController,
+                        decoration: InputDecoration(
+                          labelText: 'Nome',
+                          enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(100),
+                              borderSide: BorderSide(
+                                  width: 2, color: Color(0xFF1565C0))),
+                          focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(100),
+                              borderSide: BorderSide(
+                                  width: 2, color: Color(0xFF1565C0))),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 25,
+                      ),
+                      Container(
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            _auth.registerWithEmailAndPassword(
+                                _emailController.text,
+                                _passwordController.text,
+                                _nameController.text);
+                            Navigator.pop(context);
+                          },
+                          child: Text(
+                            'REGISTRAR',
+                            style: TextStyle(
+                                fontSize: 20, color: Color(0xFFFFFFFF)),
+                          ),
+                          style: ElevatedButton.styleFrom(
+                              elevation: 0,
+                              backgroundColor: Color(0xFF1565C0),
+                              padding: EdgeInsets.all(10),
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(100),
+                                  side: BorderSide(
+                                      width: 2, color: Color(0xFF1565C0)))),
+                        ),
+                      ),
+                      Padding(
+                          padding: EdgeInsets.all(20),
+                          child: Text(
+                            'Ou faça login',
+                            style: TextStyle(
+                                fontSize: 24, fontWeight: FontWeight.w600),
+                          )),
+                      Container(
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          onPressed: () => Navigator.pop(context),
+                          child: Text(
+                            'LOGIN',
+                            style: TextStyle(
+                                fontSize: 20, color: Color(0xFF1565C0)),
+                          ),
+                          style: ElevatedButton.styleFrom(
+                              elevation: 0,
+                              backgroundColor: Colors.transparent,
+                              padding: EdgeInsets.all(10),
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(100),
+                                  side: BorderSide(
+                                      width: 2, color: Color(0xFF1565C0)))),
+                        ),
+                      ),
+                    ],
+                  ),
+                ))));
   }
 }
